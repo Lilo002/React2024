ad29e17f1aa0fb43d3eac5d0ab26e6bd653fdc91
Lilo002 <lisabasarab@gmail.com>
2024-07-01 19:05:47 +0300

    chore: configure linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,14 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react-compiler'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-implicit-any': 'error',
+    'react-compiler/react-compiler': 'error',
   },
 }
