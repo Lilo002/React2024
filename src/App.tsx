4efd34ec31e96e6290349ee1bae60d36532a2c91
Lilo002 <lisabasarab@gmail.com>
2024-07-03 14:45:57 +0300

    feat: fetch data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Component } from 'react';
 import { SearchFiled } from './components/searchField/SearchFiled';
 import { ResultField } from './components/resultField/ResultField';
+import { ResponseItem } from './types';
+
+const URL = 'https://pokeapi.co/api/v2/';
+const LIMIT = 20;
 
 interface AppState {
   inputValue: string;
-  results: { name: string; description: string }[];
+  results: ResponseItem[];
 }
 
 class App extends Component<unknown, AppState> {
@@ -20,7 +24,30 @@ class App extends Component<unknown, AppState> {
     const trimmedValue = value.trim();
     this.setState({ inputValue: trimmedValue });
     localStorage.setItem('Lilo-value', trimmedValue);
+    this.fetchData(value);
   };
+
+  fetchData = (value: string) => {
+    value.trim() ? this.fetchSearchedData(value) : this.fetchAllData();
+  };
+
+  fetchAllData = () =>
+    fetch(`${URL}pokemon?limit=${LIMIT}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ results: data.results });
+        console.log(data.results);
+      })
+      .catch((error) => console.log(error));
+
+  fetchSearchedData = (value: string) =>
+    fetch(`${URL}pokemon/${value.trim()}/`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ results: [data] });
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
 
   render() {
     const { inputValue, results } = this.state;
