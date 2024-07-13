2ee0d3b7fd0b99117cce358f6ce67b9e715ce7d8
Lilo002 <lisabasarab@gmail.com>
2024-07-13 09:06:26 +0300

    refactor: rewrite card component

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,48 +1,12 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
 import { SearchItem } from '../../types';
 
-import './_style.scss';
-import { URL } from '../../constant';
+type CardProps = {
+  data: SearchItem;
+};
 
-export function Card({ nameValue }: { nameValue: string }) {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [data, setData] = useState<null | SearchItem>(null);
-
-  const fetchSearchedData = async (value: string): Promise<SearchItem> => {
-    const response = await fetch(`${URL}pokemon/${value.trim().toLowerCase()}/`);
-    const data = await response.json();
-
-    return data;
-  };
-
-  const setFetchedData = useCallback(async () => {
-    try {
-      const data = await fetchSearchedData(nameValue);
-      setData(data);
-    } catch (error) {
-      console.log(error);
-      setData(null);
-    }
-  }, [nameValue]);
-
-  useEffect(() => {
-    setFetchedData();
-  }, [setFetchedData]);
-
-  const handleImageLoad = () => {
-    setIsLoaded(true);
-  };
-
-  if (!data) {
-    return (
-      <div className="card">
-        <div className="loader-container">
-          <div className="card-loader"></div>
-        </div>
-      </div>
-    );
-  }
-
+export function Card({ data }: CardProps) {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
   const {
     name,
     sprites: {
@@ -55,16 +19,20 @@ export function Card({ nameValue }: { nameValue: string }) {
     height,
   } = data;
 
+  const handleImageLoad = () => {
+    setIsImageLoaded(true);
+  };
+
   return (
     <div className="card">
       <div className="card-img-container">
-        {!isLoaded && (
+        {!isImageLoaded && (
           <div className="loader-container">
             <div className="card-loader"></div>
           </div>
         )}
         <img
-          className={`card-img ${isLoaded ? 'loaded' : ''}`}
+          className={`card-img ${isImageLoaded ? 'loaded' : ''}`}
           src={front_default}
           alt={name}
           onLoad={handleImageLoad}
