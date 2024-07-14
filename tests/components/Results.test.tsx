021efbe04b8038350f7a812fe7bdce46572c1d03
Lilo002 <lisabasarab@gmail.com>
2024-07-14 18:23:16 +0300

    feat: add results test

---
+++ tests/components/Results.test.tsx
@@ -0,0 +1,51 @@
+import { describe, it, expect, vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import { ResultField } from '../../src/components/resultField/ResultField';
+import { BrowserRouter } from 'react-router-dom';
+import { resultsMock } from '../mocks';
+
+describe('ResultField', () => {
+  const mockFetch = vi.fn();
+
+  beforeEach(() => {
+    globalThis.fetch = mockFetch;
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('show loader if data is loading', async () => {
+    vi.mocked(fetch).mockImplementation(() => new Promise(() => {}));
+
+    render(
+      <BrowserRouter>
+        <ResultField />
+      </BrowserRouter>,
+    );
+
+    expect(screen.getByTestId('loader')).toBeDefined();
+    expect(screen.queryByTestId('results-catalog')).toBeNull();
+  });
+
+  it('show card with data and remove loader if data loaded', async () => {
+    vi.mocked(fetch).mockResolvedValueOnce({
+      json: async () => ({ results: resultsMock }),
+    } as Response);
+
+    render(
+      <BrowserRouter>
+        <ResultField />
+      </BrowserRouter>,
+    );
+
+    await waitFor(() => {
+      const listItems = screen.getAllByRole('link');
+      listItems.forEach((item, index) => {
+        expect(item).toHaveTextContent(resultsMock[index].name);
+      });
+    });
+
+    expect(screen.queryByTestId('loader')).toBeNull();
+  });
+});
