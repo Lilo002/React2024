d1044439884e250afd9f31d9774021eecf4cdc94
Lilo002 <lisabasarab@gmail.com>
2024-07-19 17:07:11 +0300

    feat: add theme provider to app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Layout } from '../layout/layout';
 import { Details } from '../components/details/details';
 import { ErrorPage } from '../components/404/404';
+import { ThemeProvider } from '../components/themeProvider/themeProvider';
 
 export function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="pokemon/:id" element={<Details />} />
-        </Route>
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="pokemon/:id" element={<Details />} />
+          </Route>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
---
+++ src/components/themeProvider/themeProvider.tsx
@@ -0,0 +1,19 @@
+import { ReactNode, useState } from 'react';
+import { ThemeContext, ToggleThemeContext } from '../../context/context';
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  const [lightTheme, setLightTheme] = useState(false);
+
+  const toggleTheme = () => {
+    console.log('switch');
+    setLightTheme((prev) => !prev);
+  };
+
+  return (
+    <ThemeContext.Provider value={lightTheme}>
+      <ToggleThemeContext.Provider value={toggleTheme}>
+        {children}
+      </ToggleThemeContext.Provider>
+    </ThemeContext.Provider>
+  );
+}
