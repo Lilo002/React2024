9fb49c3dd11fec5157961999ee2485088931c0fb
Lilo002 <lisabasarab@gmail.com>
2024-07-24 19:58:34 +0300

    refactor: replace fetch by api redux

--- src/components/resultField/ResultField.tsx
+++ src/components/resultField/ResultField.tsx
@@ -1,50 +1,38 @@
-import { ResponseItem, SearchItem } from '../../types';
-
 import './_style.scss';
-import { MouseEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { LIMIT, OFFSET, URL } from '../../constant';
+import { MouseEvent, useMemo, useRef } from 'react';
+import { LIMIT } from '../../constant';
 import { useNavigateMethods } from '../../hooks/useNavigateMethods';
 import { Buttons } from '../buttons/buttons';
 import { List } from '../list/list';
 import { Loader } from '../loader/loader';
+import { useGetAllPokemonQuery, useGetPokemonByNameQuery } from '../../store/api/api';
+import { Flyout } from '../flyout/flyout';
 
 export function ResultField() {
   const { navigateToMainPage, getPageValue, getSearchValue } = useNavigateMethods();
 
-  const [results, setResults] = useState<null | SearchItem[] | ResponseItem[]>(null);
-  const [isDataLoaded, setIsDataLoaded] = useState(false);
-
   const list = useRef(null);
 
   const page = useMemo(() => getPageValue(), [getPageValue]);
+  const searchValue = useMemo(() => getSearchValue().trim().toLowerCase(), [getSearchValue]);
 
-  const fetchAllData = useCallback(async (): Promise<ResponseItem[]> => {
-    const response = await fetch(`${URL}pokemon?limit=${LIMIT}&offset=${(page - 1) * OFFSET}`);
-    const data = await response.json();
-    const results = data.results;
-    return results;
-  }, [page]);
+  const { data: allPokemon, isFetching: isAllLoading } = useGetAllPokemonQuery(page);
+  const {
+    data: searchedPokemon,
+    isFetching: isSearchLoading,
+    isError: isSearchError,
+  } = useGetPokemonByNameQuery(searchValue, {
+    skip: !searchValue,
+  });
 
-  const fetchSearchedData = async (value: string): Promise<SearchItem[]> => {
-    const response = await fetch(`${URL}pokemon/${value.trim().toLowerCase()}/`);
-    const data = await response.json();
-    return [data];
-  };
+  const results = useMemo(() => {
+    if (searchValue) {
+      return isSearchError ? [] : searchedPokemon ? [searchedPokemon] : null;
+    }
+    return allPokemon ? allPokemon : null;
+  }, [searchValue, searchedPokemon, allPokemon, isSearchError]);
 
-  const fetchData = useCallback(
-    async (value: string) => {
-      try {
-        const data = value.trim() ? await fetchSearchedData(value) : await fetchAllData();
-        if (data) setResults(data);
-        setIsDataLoaded(true);
-      } catch (error) {
-        setResults(null);
-        setIsDataLoaded(true);
-        console.error(error);
-      }
-    },
-    [fetchAllData],
-  );
+  const isDataLoaded = !isAllLoading && !isSearchLoading;
 
   const returnToMainPage = (e: MouseEvent<HTMLElement>) => {
     if (e.target instanceof Node && e.target.contains(list.current)) {
@@ -55,11 +43,6 @@ export function ResultField() {
   const isPrevBtnDisabled = () => !isDataLoaded || !results || page === 1;
 
   const isNextBtnDisabled = () => !isDataLoaded || !results || !!(results && results.length < LIMIT);
-
-  useEffect(() => {
-    setIsDataLoaded(false);
-    fetchData(getSearchValue());
-  }, [fetchData, getSearchValue]);
 
   return (
     <div className="results" onClick={returnToMainPage}>
@@ -73,6 +56,7 @@ export function ResultField() {
         )}
       </div>
       <Buttons isNextBtnDisabled={isNextBtnDisabled()} isPrevBtnDisabled={isPrevBtnDisabled()} />
+      <Flyout />
     </div>
   );
 }
