c336a05ac367fdee4fd0918a3d59f1fdde285348
Lilo002 <lisabasarab@gmail.com>
2024-07-26 18:34:53 +0300

    feat: add test for list component

--- tests/components/List.test.tsx
+++ tests/components/List.test.tsx
@@ -1,40 +1,77 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import { ListItem } from '../../src/components/listItem/listItem';
 import { List } from '../../src/components/list/list';
 import { BrowserRouter } from 'react-router-dom';
-import { detailedDataMock, resultsMock } from '../mocks';
+import { handlers, resultsMock } from '../mocks';
 import { App } from '../../src/app/App';
+import { setupServer } from 'msw/node';
+import { renderWithProviders } from '../test-utils';
+
+const server = setupServer(...handlers);
+
+describe('Navigation and Details Rendering', () => {
+  beforeAll(() => server.listen());
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => server.close());
+
+  it('should navigate to Details and render data on link click', async () => {
+    renderWithProviders(<App />);
+
+    const listItemLink = await screen.findByText(resultsMock[0].name);
+
+    fireEvent.click(listItemLink);
+
+    expect(screen.queryByText('details')).toBeNull();
+  });
+});
 
 describe('ListItem render', () => {
-  it('should render listItem with data', () => {
-    render(
+  beforeAll(() => server.listen());
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => server.close());
+  it('should render listItem with data', async () => {
+    renderWithProviders(
       <BrowserRouter>
         <ListItem data={resultsMock[0]} />
       </BrowserRouter>,
     );
 
-    const item = screen.getByRole('listitem');
-    expect(item).toBeInTheDocument();
-    expect(screen.getByText(resultsMock[0].name)).toBeInTheDocument;
+    await waitFor(() => {
+      const item = screen.getByRole('listitem');
+      expect(item).toBeInTheDocument();
+      expect(screen.getByText(resultsMock[0].name)).toBeInTheDocument;
 
-    const link = screen.getByRole('link');
-    expect(link).toBeInTheDocument();
-    expect(link).toHaveAttribute('href', expect.stringContaining(`/pokemon/${resultsMock[0].name}`));
+      const link = screen.getByRole('link');
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute('href', expect.stringContaining(`/pokemon/${resultsMock[0].name}`));
+    });
   });
 });
 
 describe('List render', () => {
-  it('should render list item', () => {
-    render(
+  beforeAll(() => server.listen());
+
+  afterEach(() => server.resetHandlers());
+
+  afterAll(() => server.close());
+
+  it('should render list item', async () => {
+    renderWithProviders(
       <BrowserRouter>
         <List results={resultsMock} />
       </BrowserRouter>,
     );
 
-    const list = screen.getByRole('list');
-    const items = screen.getAllByRole('listitem');
-    expect(list).toBeInTheDocument();
-    expect(items.length).equal(resultsMock.length);
+    await waitFor(() => {
+      const list = screen.getByRole('list');
+      const items = screen.getAllByRole('listitem');
+      expect(list).toBeInTheDocument();
+      expect(items.length).equal(resultsMock.length);
+    });
 
     resultsMock.forEach((result) => {
       const link = screen.getByRole('link', { name: result.name });
@@ -43,80 +80,21 @@ describe('List render', () => {
     });
   });
 
-  it('should render loader if there is no data', () => {
-    render(
+  it('should render loader if there is no data', async () => {
+    renderWithProviders(
       <BrowserRouter>
         <List results={null} />
       </BrowserRouter>,
     );
 
-    const list = screen.getByRole('list');
-    const items = screen.queryAllByRole('listitem');
-    const errorMessage = screen.getByText(/Oops, this Pokémon doesn't exist yet/i);
-
-    expect(list).toBeInTheDocument();
-    expect(items.length).toEqual(0);
-    expect(errorMessage).toBeInTheDocument();
-  });
-});
-
-vi.mock('react-router-dom', async () => {
-  const actual = await vi.importActual('react-router-dom');
-  return {
-    ...actual,
-    useParams: () => ({ id: detailedDataMock.name }),
-    useNavigate: () => vi.fn(),
-  };
-});
-
-describe('Navigation and Details Rendering', () => {
-  const mockFetch = vi.fn();
-
-  beforeEach(() => {
-    globalThis.fetch = mockFetch;
-  });
-
-  afterEach(() => {
-    vi.resetAllMocks();
-  });
-
-  it('should navigate to Details and render data on link click', async () => {
-    mockFetch.mockResolvedValueOnce({
-      json: () => Promise.resolve({ results: [resultsMock[0]] }),
-    });
-
-    render(<App />);
-
-    const listItemLink = await screen.findByText(resultsMock[0].name);
-
-    fireEvent.click(listItemLink);
-
     await waitFor(() => {
-      expect(screen.getByTestId('details')).toBeDefined();
+      const list = screen.getByRole('list');
+      const items = screen.queryAllByRole('listitem');
+      const errorMessage = screen.getByText(/Oops, this Pokémon doesn't exist yet/i);
+
+      expect(list).toBeInTheDocument();
+      expect(items.length).toEqual(0);
+      expect(errorMessage).toBeInTheDocument();
     });
-  });
-
-  it('should navigate to Details and start fetch data and render component with this', async () => {
-    mockFetch
-      .mockResolvedValueOnce({
-        json: () => Promise.resolve({ results: [resultsMock[0]] }),
-      })
-      .mockResolvedValueOnce({
-        json: () => Promise.resolve(detailedDataMock),
-      });
-
-    render(<App />);
-
-    const listItemLink = await screen.findByText(resultsMock[0].name);
-    expect(listItemLink).toBeDefined();
-
-    fireEvent.click(listItemLink);
-
-    await waitFor(() => {
-      expect(screen.getByTestId('card')).toBeDefined();
-    });
-
-    expect(screen.getByText(detailedDataMock.name)).toBeDefined();
-    expect(screen.getByText(detailedDataMock.types[0].type.name)).toBeDefined();
   });
 });
