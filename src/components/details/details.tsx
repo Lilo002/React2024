bae2194b69a93a178695a0848ae056fd681e1edf
Lilo002 <lisabasarab@gmail.com>
2024-07-27 08:24:33 +0300

    fix: replace card by Card in imports

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -1,7 +1,7 @@
 import './_style.scss';
 import { Link, useParams } from 'react-router-dom';
 import { useNavigateMethods } from '../../hooks/useNavigateMethods';
-import { Card } from '../card/card';
+import { Card } from '../card/Card';
 import { Loader } from '../loader/loader';
 import { useGetPokemonByNameQuery } from '../../store/api/api';
 
