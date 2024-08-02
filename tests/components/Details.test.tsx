05f882c6f9e25fbe6697bd85523c993a91fb14e2
Lilo002 <lisabasarab@gmail.com>
2024-08-02 10:19:06 +0300

    feat: add tests for card and details components

--- tests/components/Details.test.tsx
+++ tests/components/Details.test.tsx
@@ -1,61 +1,66 @@
 import { screen, waitFor, fireEvent } from '@testing-library/react';
-import { Details } from '../../src/components/details/details';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { handlers, resultsMock } from '../mocks';
-import { App } from '../../src/app/App';
+import { detailedDataMock, handlers } from '../mocks';
 import { setupServer } from 'msw/node';
 import { renderWithProviders } from '../test-utils';
+import { describe, beforeAll, afterEach, afterAll, it, expect, vi } from 'vitest';
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider';
 
+import Details from '../../src/pages/pokemon/[id]';
+import mockRouter from 'next-router-mock';
+
+import { createDynamicRouteParser } from 'next-router-mock/dynamic-routes';
+
+vi.mock('next/router', () => require('next-router-mock'));
 const server = setupServer(...handlers);
 
-describe('Details', () => {
-  beforeAll(() => server.listen());
+mockRouter.useParser(createDynamicRouteParser(['/pokemon/[id]']));
+
+describe('Flyout', () => {
+  beforeAll(() => {
+    server.listen();
+  });
 
   afterEach(() => server.resetHandlers());
 
   afterAll(() => server.close());
+
   it('show loader when data is loading', async () => {
-    renderWithProviders(<App />);
+    renderWithProviders(<Details />);
+    mockRouter.push('/pokemon/bulbasaur');
 
-    const listItemLink = await screen.findByText(resultsMock[0].name);
-    expect(screen.queryByTestId('card')).toBeNull();
-    expect(listItemLink).toBeDefined();
-
-    fireEvent.click(listItemLink);
-
-    expect(screen.getByTestId('loader')).toBeDefined();
+    expect(screen.getAllByTestId('loader')).toBeDefined();
     expect(screen.queryByTestId('card')).toBeNull();
   });
 
   it('close details when clicked on close button', async () => {
-    renderWithProviders(<App />);
+    renderWithProviders(
+      <MemoryRouterProvider url="/pokemon/bulbasaur">
+        <Details />
+      </MemoryRouterProvider>,
+    );
 
-    const listItemLink = await screen.findAllByText(resultsMock[0].name);
-    expect(listItemLink).toBeDefined();
-    fireEvent.click(listItemLink[0]);
+    expect(screen.getAllByTestId('loader')).toBeDefined();
 
-    await waitFor(() => {
+    waitFor(() => {
       expect(screen.getByTestId('details')).toBeDefined();
     });
 
     const closeButton = screen.getByTestId('card-close');
     fireEvent.click(closeButton);
 
-    await waitFor(() => {
-      expect(screen.queryByTestId('details')).toBeNull();
+    waitFor(() => {
+      expect(screen.queryByText(detailedDataMock.types[0].type.name)).toBeNull();
     });
   });
 
   it('render empty block if  there is an Error in fetch', async () => {
     renderWithProviders(
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Details />} />
-        </Routes>
-      </BrowserRouter>,
+      <MemoryRouterProvider url="/">
+        <Details />
+      </MemoryRouterProvider>,
     );
 
-    await waitFor(() => {
+    waitFor(() => {
       expect(screen.queryByTestId('card')).toBeNull();
       expect(screen.queryByTestId('detailed-loader')).toBeNull();
     });
