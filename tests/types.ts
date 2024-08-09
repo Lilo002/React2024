7d7bda231b5fd73bbe2d0ca1aa07e5efd7f5dc39
Lilo002 <lisabasarab@gmail.com>
2024-08-09 21:05:10 +0300

    refactor: delete files

--- tests/types.ts
+++
@@ -1,4 +0,0 @@
-import { pokemonApi } from '../src/store/api/api';
-
-export type UseGetPokemonByNameQueryResult = ReturnType<typeof pokemonApi.useGetPokemonByNameQuery>;
-export type UseGetAllPokemonQueryResult = ReturnType<typeof pokemonApi.useGetAllPokemonQuery>;
