97d9afe9fe7506af0edb5180028a2125c8d75342
Lilo002 <lisabasarab@gmail.com>
2024-08-15 22:39:28 +0300

    feat: add formDataToUser function

--- src/utils/convert/convert.ts
+++ src/utils/convert/convert.ts
@@ -1,3 +1,5 @@
+import { User } from '../../types';
+
 export const convertTo64 = (file: File): Promise<string> => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -17,4 +19,63 @@ export const convertTo64 = (file: File): Promise<string> => {
     };
     reader.readAsArrayBuffer(file);
   });
+};
+
+export const formDataToUser = async (
+  elements: HTMLFormControlsCollection
+): Promise<User> => {
+  const user: User = {
+    name: '',
+    age: 0,
+    email: '',
+    password: '',
+    passwordRepeat: '',
+    gender: null,
+    terms: false,
+    photo: undefined,
+    country: '',
+  };
+
+  for (const key in user) {
+    const element = elements.namedItem(key) as
+      | HTMLInputElement
+      | HTMLSelectElement
+      | RadioNodeList;
+    if (element) {
+      switch (key) {
+        case 'name':
+          user.name = element.value;
+          break;
+        case 'age':
+          user.age = parseInt(element.value, 10) || 0;
+          break;
+        case 'email':
+          user.email = element.value;
+          break;
+        case 'password':
+          user.password = element.value;
+          break;
+        case 'passwordRepeat':
+          user.passwordRepeat = element.value;
+          break;
+        case 'gender':
+          user.gender = element.value as 'male' | 'female';
+          break;
+        case 'terms':
+          user.terms = (element as HTMLInputElement).checked;
+          break;
+        case 'photo':
+          if (element instanceof HTMLInputElement && element.type === 'file') {
+            if (element.files && element.files.length > 0) {
+              user.photo = element.files;
+            }
+          }
+          break;
+        case 'country':
+          user.country = element.value;
+          break;
+      }
+    }
+  }
+  return user;
 };
